f2ead5332d32c9f72d086d0d06f7233ee3121158
leongck1 <38928795+leongck1@users.noreply.github.com>
2022-08-11 21:08:02 +0800

    Update Two Sum.py

--- Easy/Two Sum.py
+++ Easy/Two Sum.py
@@ -1,1 +1,11 @@
-
+# Approach 1
+# Time Complexity = O(n) where a for loop to loop through n data
+# Space Complexity = O(n) where values are stores in hash table
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited_values = dict()
+        for index, value in enumerate(nums):
+            if target - value in visited_values:
+                return [visited_values[target - value], index]
+            else:
+                visited_values[value] = index
